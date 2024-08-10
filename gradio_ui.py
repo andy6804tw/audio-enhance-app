f54f3089967d79d38dd938f3e2277e63316a4455
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2024-08-10 08:27:36 +0800

    modify

--- gradio_ui.py
+++ gradio_ui.py
@@ -2,7 +2,7 @@ import gradio as gr
 import torch
 import torchaudio
 
-from resemble_enhance.enhancer.inference import denoise, enhance
+from resemble_enhance.enhancer.inference import  enhance
 
 if torch.cuda.is_available():
     device = "cuda"
